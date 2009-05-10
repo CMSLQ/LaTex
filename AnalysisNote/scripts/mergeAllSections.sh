f9bc6d376770de083c55f92ec6b99e375421bc50
prumerio <>
2009-05-10 17:17:49 +0000

    merge and create pdf

--- AnalysisNote/scripts/mergeAllSections.sh
+++ AnalysisNote/scripts/mergeAllSections.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
-OUTFILE=CMS_AN_2008_XXX.tex
-TEXFILES="beginning.tex trig.tex elec.tex jet.tex eventSel.tex backgrnd_signal.tex signalExtraction.tex cmsPotential.tex end.tex bib.tex"
+OUTFILE=CMS_AN_2009_XXX
+TEXFILES="beginning.tex monteCarloSamples.tex trig.tex elec.tex jet.tex eventSel.tex backgrnd_signal.tex Systematics.tex cmsPotential.tex end.tex bib.tex"
 
 echo "Merging files: $TEXFILES"
 echo "               into the output file $OUTFILE"
 
-cat > $OUTFILE <<EOF
+cat > ${OUTFILE}.tex <<EOF
 \documentclass[colclass=cmspaper]{combine}
 \usepackage{lineno}
+\usepackage{amsfonts,amsmath,amssymb}
+\usepackage[dvips]{graphicx}
+\usepackage{bm}
 \begin{document}
 \begin{linenumbers}
 \pagestyle{combine}
 EOF
 
-cat $TEXFILES >> $OUTFILE
+cat $TEXFILES >> ${OUTFILE}.tex
 
-cat >> $OUTFILE <<EOF
+cat >> ${OUTFILE}.tex <<EOF
 \end{linenumbers}
 \end{document}
 EOF
 
+latex ${OUTFILE}.tex
+dvipdf ${OUTFILE}.dvi ${OUTFILE}.pdf
